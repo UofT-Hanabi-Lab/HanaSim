f53e81a7e8f95ae72d4af39fce53d292209254e4
muzammil004 <m.muzammil@mail.utoronto.ca>
2024-07-28 18:14:32 -0400

    Changed script to conform to new argument usage

--- scripts/bots_documentation.sh
+++ scripts/bots_documentation.sh
@@ -8,9 +8,13 @@ num_players=(2 3 4 5)
 for bot in ${bots[@]};
 do
     echo "Performance for $bot :"
-    for num_p in ${num_players[@]};
-    do
-        echo "-- $num_p players --"
-        ./../build/HanaSim 1000 $num_p $bot
-    done
+    echo "-- 2 players --"
+    ./../build/HanaSim 1000 2 $bot $bot
+    echo "-- 3 players --"
+    ./../build/HanaSim 1000 3 $bot $bot $bot
+    echo "-- 4 players --"
+    ./../build/HanaSim 1000 4 $bot $bot $bot $bot
+    echo "-- 5 players --"
+    ./../build/HanaSim 1000 5 $bot $bot $bot $bot $bot
+
 done
