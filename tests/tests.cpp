8ef248fb1a5e46c65cdaa20acef80e8fb48539a4
Danx1129 <danielxu1129@gmail.com>
2024-08-14 22:34:12 -0400

    changed holmes to human:

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -20,18 +20,25 @@
 #include "../src/game.cpp"
 #include "../src/move.cpp"
 #include "../src/state.cpp"
+#include "../src/humanplayer.cpp"
 
 //namespace plt = matplotlibcpp;
 
-int test_create_two_holmes_player(){
-    player* p1 = new holmesbot(0, 2);
-    player* p2 = new holmesbot(1, 2);
-    std::cout << "tested holmesbot" << std::endl;
+int test_create_two_human_player(){
+    player* p1 = new humanplayer(2, 0);
+    player* p2 = new humanplayer(2, 1);
+    std::vector<player*> players = {};
+    players.push_back(p1);
+    players.push_back(p2);
+    State init_state = State(2);
+    game newgame = game(init_state, players);
+    std::cout << "tested human player" << std::endl;
+    int score = newgame.run(true);
 }
 
 int main() {
 
     std::cout << "Tests running :)" << std::endl;
-    test_create_two_holmes_player();
+    test_create_two_human_player();
     return 0;
 }
