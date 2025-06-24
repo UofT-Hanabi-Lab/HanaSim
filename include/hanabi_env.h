5aece2bfbf52bf9990b37fa24ca93109af44f1f1
Wilson Sy <10039348+boxspah@users.noreply.github.com>
2025-06-23 20:42:34 -0400

    Format hanabi_env files

--- include/hanabi_env.h
+++ include/hanabi_env.h
@@ -9,20 +9,21 @@
 #include <execution>
 #include <omp.h>
 
-
 #include "../include/game.h"
 
-class HanabiEnv {
+class HanabiEnv
+{
 public:
     HanabiEnv(int num_players);
 
     Observation reset();
 
-    struct StepResult {
-        Observation observation; //
-        float reward;          
-        bool done;               
-        std::string info;       // additional info
+    struct StepResult
+    {
+        Observation observation;
+        float reward;
+        bool done;
+        std::string info; // additional info
     };
 
     StepResult step(const py::tuple &action);
@@ -31,9 +32,8 @@ public:
 
 private:
     int num_players_;
-    bool done_;        // mark if the game end
-    State* curr_state_ = NULL; 
-    int last_score_;   // To calculate score
+    bool done_; // mark if the game end
+    State *curr_state_ = NULL;
+    int last_score_; // To calculate score
     int curr_player_;
 };
-
