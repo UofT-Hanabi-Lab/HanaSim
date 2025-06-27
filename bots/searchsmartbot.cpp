f090afe9d4eb29f9c78f435b3721e9d53a8a0d9e
Freya Fu <ffucopyowl@gmail.com>
2025-06-27 12:52:37 -0400

    add some belief state files

---
+++ bots/searchsmartbot.cpp
@@ -0,0 +1,114 @@
+#include "include/searchsmartbot.h"
+#include "player.h"         // must include the base class
+#include "smartbot.h"
+#include <memory>
+
+//TODO: currently, search simulation happens when it's acting agent's turn.
+//Improvements:
+//      May add an upperbound of search space, and enables larger amount of simulations.
+//      if (sizeof(player1.search_space) > threshold) 
+//          {act according to blueprint policy (eg. smartbot)}
+
+class SearchSmartBot : public player {
+public:
+    SearchSmartBot(int id, int n_players, int search_n, float search_thresh);
+
+    move play(State s) override;
+    void observe(State s, move m) override;
+    move play_prechosen(State s, move m) override;
+    int get_id() override;
+    int get_n_cards() override;
+
+private:
+    int id_;                    
+    int n_players_;                 //total players
+    int search_n_;                  //number of simulations
+    float search_thresh_;           //search space upperbound
+    std::shared_ptr<smartbot> bp_;  // the blueprint policy?TODO
+
+    float simulate_game(State s);   //the MCS
+};
+
+SearchSmartBot::SearchSmartBot(int id, int n_players, int search_n, float search_thresh)
+    : id_(id), n_players_(n_players), search_n_(search_n), search_thresh_(search_thresh) {
+    bp_ = std::make_shared<smartbot>(id, n_players);
+}
+
+void SearchSmartBot::observe(State s, move m) {
+    bp_->observe(s, m);
+}
+
+move SearchSmartBot::play(State s) {
+    //only replace the move with search when search move score is higher than 
+    // bluprint score by search_thresh_ points
+    move bp_move = bp_->play(s);
+
+    std::vector<move> legal_moves = s.get_legal_moves(id_);
+    move best_move = bp_move;
+    float best_score = -1;
+
+    // Note that if the transition function is only stocastic when you shuffle the deck every time
+    // here we asssume no shuffling, so the bp_score is fixed given the start state.
+    State s_copy = s;
+    s_copy.transition(bp_move, false);
+    float bp_score = simulate_game(s_copy);
+
+    for (const move& m : legal_moves){
+
+        float score_sofar = 0;
+        for (int i=0; i<search_n_;i++){
+            State sample_state = sample_belief_state();
+            //TODO: implement the belief distribution function
+            //TODO: not the real state, but generate the state based on distribution of real state.
+            sample_state.transition(m, false);
+            score_sofar += simulate_game(sample_state);
+        }
+        float avg = score_sofar / search_n_;
+
+        if (avg>best_score){
+            best_move = m;
+            best_score = avg;
+        }
+    }
+
+    if (best_score - bp_score > search_thresh_){
+        return best_move;
+    }
+
+    return bp_move;
+}
+
+move SearchSmartBot::play_prechosen(State s, move m) {
+    return bp_->play_prechosen(s, m);
+}
+
+int SearchSmartBot::get_id() {
+    return id_;
+}
+
+int SearchSmartBot::get_n_cards() {
+    return bp_->get_n_cards();
+}
+
+float SearchSmartBot::simulate_game(State s) {
+    // Keep simulating until terminal condition is reached
+    while (s.get_num_lives() > 0 && !s.get_deck().empty()) {
+        int current_player = s.get_deck().size() % n_players_;  // basic estimation of who's turn it is
+        std::vector<move> legal_moves = s.get_legal_moves(current_player);
+        
+        if (legal_moves.empty()) break;
+
+        // Use blueprint policy to choose a move
+        move m = bp_->play(s);
+        s.transition(m, false);  // false = no logging
+    }
+
+    // Score = sum of top cards in each pile (ignoring index 0 if that's a placeholder)
+    std::vector<int> piles = s.get_piles();
+    int score = 0;
+    for (size_t i = 1; i < piles.size(); ++i) {
+        score += piles[i];
+    }
+    return static_cast<float>(score);
+}
+
---
+++ tests/test_search.cpp
@@ -0,0 +1,119 @@
+#include "../bots/include/searchsmartbot.h"
+#include "../bots/searchsmartbot.cpp"
+
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <execution>
+#include <omp.h>
+#include <random>
+#include <filesystem>
+
+#include "../include/global_int.h"
+
+#include "../bots/holmesbot.cpp"
+#include "../bots/smartbot.cpp"
+
+#include "../src/card.cpp"
+#include "../src/game.cpp"
+//
+// #include "test_game.cpp"
+#include "../src/move.cpp"
+#include "../src/state.cpp"
+#include "../src/humanplayer.cpp"
+
+
+//namespace plt = matplotlibcpp;
+
+int seed_num;
+//int game_num;
+
+void print_card(Card c){
+  	if(c.color()== 1){
+          std::cout<< "red " << c.rank() << std::endl;
+  	}
+    if(c.color()== 2){
+          std::cout<< "white " << c.rank() << std::endl;
+  	}
+    if(c.color()== 3){
+          std::cout<< "yellow " << c.rank() << std::endl;
+  	}
+    if(c.color()== 4){
+          std::cout<< "green " << c.rank() << std::endl;
+  	}
+    if(c.color()== 5){
+          std::cout<< "blue " << c.rank() << std::endl;
+  	}
+}
+
+// move create_move(move_type type, int to_player, int from_player, std::vector<int> card_indices, int color_or_rank){
+//     if(int(type) == 1){ // color hint
+//         return move(type, to_player, from_player, card_indices, (Color) color_or_rank);
+//     }
+//     if(int(type) == 2){ // rank hint
+//         return move(type, to_player, from_player, card_indices, (Rank) color_or_rank);
+//     }
+//     if(int(type) == 3 || int(type) == 4){ // play
+//         return move(type, from_player, card_indices[0]);
+//     }
+// }
+
+
+std::vector<Card> deck_ = { Card(red, one), Card(red, one), Card(red, one), Card(red, two), Card(red, two), Card(red, three), Card(red, three), Card(red, four), Card(red, four), Card(red, five),
+              Card(blue, one), Card(blue, one), Card(blue, one), Card(blue, two), Card(blue, two), Card(blue, three), Card(blue, three), Card(blue, four), Card(blue, four), Card(blue, five),
+              Card(yellow, one), Card(yellow, one), Card(yellow, one), Card(yellow, two), Card(yellow, two), Card(yellow, three), Card(yellow, three), Card(yellow, four), Card(yellow, four), Card(yellow, five),
+              Card(green, one), Card(green, one), Card(green, one), Card(green, two), Card(green, two), Card(green, three), Card(green, three), Card(green, four), Card(green, four), Card(green, five),
+              Card(white, one), Card(white, one), Card(white, one), Card(white, two), Card(white, two), Card(white, three), Card(white, three), Card(white, four), Card(white, four), Card(white, five)};
+
+std::vector<Card> test_shuffle_using_seed(int seed){
+	std::vector<Card> new_deck = deck_;
+	std::default_random_engine rng(seed);
+    std::shuffle(new_deck.begin(), new_deck.end(), rng);
+    for (int x = 0; x < new_deck.size(); x++) {
+        print_card(new_deck[x]);
+    }
+    return new_deck;
+}
+
+int test_create_singleagent_sparta_with_2_players(){
+    player* p1 = new SearchSmartBot(0, 2, /*search_n=*/10, /*search_thresh=*/0.5);
+    player* p2 = new SearchSmartBot(1, 2, 10, 0.5);
+    std::vector<player*> players = {};
+    players.push_back(p1);
+    players.push_back(p2);
+    std::vector<Card> deck = test_shuffle_using_seed(494);
+    State init_state = State(2, deck);
+    game newgame = game(init_state, players);
+    std::cout << "tested smartbot custom" << std::endl;
+
+    // custom list of moves
+    std::vector<move> test_moves = {move((move_type)3, 1, 2), move((move_type)3, 1, 2), move((move_type)3, 0, 2), 
+    move((move_type)3, 1, 2), move((move_type)3, 0, 2)};
+
+    std::reverse(test_moves.begin(), test_moves.end());
+    int score = newgame.run_test(true, test_moves);
+    return score;
+}
+
+int main() {
+    std::cout << "Tests running :)" << std::endl;
+    test_shuffle_using_seed(494);
+    seed_num = 494;
+    game_num = 1;
+    //test_create_two_human_player_custom();
+    //test_create_two_holmesbot_custom();
+    //test_create_two_smartbot_custom();
+    //test_create_two_human_player();
+    for (int x = 0; x < 1; x++){
+        test_create_two_smartbot(seed_num);
+        game_num +=1;
+        seed_num +=1; 
+    }
+    test_create_two_smartbot(seed_num);
+    // std::filesystem::remove_all("output/p1");
+    return 0;
+}
+
+//5 15 15 
